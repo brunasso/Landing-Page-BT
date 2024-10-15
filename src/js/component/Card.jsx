61d5d3daf7e3fd7c1bac9459955504bc0de1d5a3
brunasso <53584556+brunasso@users.noreply.github.com>
2024-10-15 00:09:36 +0000

    Cambios realizados?

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -3,14 +3,17 @@ import React from "react";
 //create your first component
 const Card = () => {
 	return (
-		<div className="card col-12 col-md-3 mb-4 border-0" style={{width: "18rem"}}>
-			<img src="https://placehold.co/530x325" className="card-img-top" alt="..."/>
+		<div className="card col-12 col-md-3 mb-4 p-0" style={{width: "18rem"}}>
+			<img src="https://placehold.co/530x325" className="card-img-top" alt="..." />
 			<div className="card-body text-center">
 				<h5 className="card-title">Card title</h5>
 				<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+			</div>
+			<div className="border-top text-center p-3 bg-light">
 				<a href="#" className="btn btn-primary">Go somewhere</a>
-			</div>		
-		</div>	
+			</div>
+		</div>
+
 	);
 };
 
